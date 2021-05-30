3be72896ac8bd9b1783bbed1959194b48f38f5c0
DESKTOP-BO7VQMC\rbals <rbalsdl008@naver.com>
2021-05-30 18:27:56 +0900

    지금까지 만들었던 todo프로젝트 최적화를 진행

--- src/App.js
+++ src/App.js
@@ -59,10 +59,12 @@ const App = () => {
                 //  '==' 연산자를 이용하여 서로 다른 유형의 두 변수의 [값] 비교
                 //  '==='는 엄격한 비교를 하는 것으로 알려져 있다 ([값 & 자료형] -> true).
                 todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo))
+                
             );
         },
         [todos]
     );
+
 
     return (
         <TodoTemplate>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Appcopy from './Appcopy';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Appcopy/>
+        {/* <App /> */}
     </React.StrictMode>,
     document.getElementById('root')
 );
